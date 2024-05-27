6f9f939d561294b470d1c51bac38ecea685192be
Blakthorne <dlpolar38@gmail.com>
2024-05-27 17:05:29 -0400

    add state and label to diagram component

--- components/Diagram.tsx
+++ components/Diagram.tsx
@@ -1,16 +1,25 @@
-import Image from "next/image"
+import Image from "next/image";
+import { useState, useEffect } from "react";
 import { useTheme } from "nextra-theme-docs";
 
-export default function Diagram({ altText, imageName }) {
+/**
+ * Render a formatted diagram given the source file and alt text to display
+ * 
+ * @param alText - the alt text
+ * @param imageName - the base name of the diagram (wihout leading slash, extension, or theme designation)
+ */
+export default function Diagram({ altText, imageName, label }) {
     
+    // Get whether theme is `light` or `dark`
     const { resolvedTheme } = useTheme();
 
-    if (resolvedTheme === 'dark') {
-        imageName = "/diagrams/" + imageName + "_dark.png";
-    }
-    else {
-        imageName = "/diagrams/" + imageName + "_light.png";
-    }
+    // Create state for the name of the image
+    const [image, setImage] = useState<string>("");
+
+    // Whenever the theme changes, switch the corresponding file with `_light` or `_dark` in the name
+    useEffect(() => {
+        setImage("/diagrams/" + imageName + "_"  + resolvedTheme + ".png");
+    }, [resolvedTheme]);
 
     return (
         <div style={{
@@ -21,12 +30,20 @@ export default function Diagram({ altText, imageName }) {
                 marginLeft: "16px",
                 marginRight: "16px"
         }}>
-            <Image
-                src={imageName}
-                alt={altText}
-                width={700}
-                height={700}
-            />
+            <div>
+                {image !== "" ?
+                    <Image
+                        src={image}
+                        alt={altText}
+                        width={700}
+                        height={700}
+                    /> : null
+                }
+                <div style={{
+                    textAlign: "center",
+                    marginTop: "8px"
+                }}>{label}</div>
+            </div>
         </div>
     )
 }
