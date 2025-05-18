cd54e66c9d0e7c8a84b9e0f3809fd1490f6133e5
Blakthorne <dlpolar38@gmail.com>
2025-05-18 17:31:03 -0400

    standardized emphasis marks and created service start page

--- app/soundbooth_checklist/_meta.jsx
+++ app/soundbooth_checklist/_meta.jsx
@@ -1,5 +1,6 @@
 export default {
     equipment_setup: "Equipment Setup",
     computer_preparation: "Computer Preparation",
+    service_start: "Service Start",
     equipment_teardown: "Equipment Teardown"
 }
