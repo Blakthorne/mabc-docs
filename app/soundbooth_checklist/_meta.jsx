a8f4c94362b6bf077cb0df32c668c2da027bb0ca
Blakthorne <dlpolar38@gmail.com>
2025-05-27 22:38:46 -0400

    added service start, during service, after service, and started on the setup for next service page

--- app/soundbooth_checklist/_meta.jsx
+++ app/soundbooth_checklist/_meta.jsx
@@ -2,5 +2,8 @@ export default {
     equipment_setup: "Equipment Setup",
     computer_preparation: "Computer Preparation",
     service_start: "Service Start",
+    during_service: "During Service",
+    after_service: "After Service",
+    setup_for_next_service: "Setup for Next Service",
     equipment_teardown: "Equipment Teardown"
 }
